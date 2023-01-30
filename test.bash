e2d8f2a1604ca2f13d108535bbd82b511c2d33e9
Kunal Mazumdar <kunalm@jfrog.com>
2023-01-30 08:25:08 +0530

    yml added

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-texts=("abc" "de f g h")
+texts=("plugin-repo-1/Screenshot 2023-01-24 at 9.13.03 AM.png" "plugin-repo-1/Screenshot 2023-01-20 at 4.24.32 PM.png")
 for text in "${texts[@]}"
 do
     echo $text
